f01cb9419c2ff9a4c87ddd0f26cebdc388640810
Renato Rocha <renatorocha@Suportes-MacBook-Pro.local>
2021-06-15 00:38:21 -0300

    Sun moon animation

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Dimensions, StyleSheet, View } from 'react-native';
@@ -8,33 +9,39 @@ import Animated, {
   useAnimatedProps,
   useSharedValue,
   withTiming,
-  interpolateColors,
   interpolateColor,
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
 } from 'react-native-reanimated';
+
 import { interpolatePath, parse } from 'react-native-redash';
 import Svg, { Path } from 'react-native-svg';
+import {
+  HandlerStateChangeEvent,
+  TapGestureHandler,
+  TapGestureHandlerEventPayload,
+} from 'react-native-gesture-handler';
 
-const { width, height } = Dimensions.get('screen');
+const { width } = Dimensions.get('screen');
 
 const AnimatedPath = Animated.createAnimatedComponent(Path);
 
 const R = 32.5;
+const MOON_RADIUS = 40;
+const CONTAINER_SIZE = 65;
+const PADDING_HORIZONTAL = 15;
+const TRANSLATE_X_END =
+  width - PADDING_HORIZONTAL * 2 - (CONTAINER_SIZE - MOON_RADIUS / 2);
 
-export default function App() {
-  const progress = useSharedValue(0);
+type ThemeState = 'light' | 'dark';
 
-  progress.value = withTiming(2, {
-    duration: 2000,
-    easing: Easing.inOut(Easing.linear),
-  });
-
-  const sunPath = `
+const sunPath = `
     M ${0}, ${32.5}
     a ${R},${R} 0 1,0 ${R * 2},0
     a ${R},${R} 0 1,0 ${-1 * (R * 2)},0
   `;
 
-  const moonPath = `
+const moonPath = `
     M ${34}, ${0}
     A ${40},${40} 
     0 0 0,
@@ -45,20 +52,39 @@ export default function App() {
     ${34} 0
   `;
 
-  const p1 = parse(sunPath);
-  const p2 = parse(moonPath);
+const p1 = parse(sunPath);
+const p2 = parse(moonPath);
+
+export default function App() {
+  const progress = useSharedValue(0);
+  const themeState = useSharedValue<ThemeState>('light');
+
+  const onHandlerStateChange = useAnimatedGestureHandler<
+    HandlerStateChangeEvent<TapGestureHandlerEventPayload>
+  >({
+    onEnd: () => {
+      themeState.value = themeState.value === 'light' ? 'dark' : 'light';
+    },
+  });
 
   const animatedProps = useAnimatedProps(() => {
     const d = interpolatePath(
       progress.value,
-      [0, 2],
+      [0, TRANSLATE_X_END],
       [p1, p2],
       Extrapolate.CLAMP,
     );
 
     const fill = interpolateColor(
       progress.value,
-      [0, 0.2, 0.68999, 0.7, 0.999, 1],
+      [
+        0,
+        TRANSLATE_X_END / 4,
+        TRANSLATE_X_END / 3,
+        TRANSLATE_X_END / 2,
+        TRANSLATE_X_END / 2.3,
+        TRANSLATE_X_END,
+      ],
       ['#FDB813', '#FDB813', '#8d6508', '#473302', '#332400', '#1b1b1b'],
     );
 
@@ -68,13 +94,30 @@ export default function App() {
     };
   });
 
+  const style = useAnimatedStyle(() => {
+    const direction = themeState.value === 'light' ? 0 : TRANSLATE_X_END;
+
+    progress.value = withTiming(direction, {
+      duration: 1500,
+      easing: Easing.inOut(Easing.linear),
+    });
+
+    return { transform: [{ translateX: progress.value }] };
+  });
+
   return (
     <View style={styles.container}>
-      <View style={styles.button_container}>
-        <Svg width={65} height={65}>
-          <AnimatedPath animatedProps={animatedProps} />
-        </Svg>
-      </View>
+      <Animated.View style={styles.button_container}>
+        <TapGestureHandler onHandlerStateChange={onHandlerStateChange}>
+          <Animated.View
+            style={[{ width: CONTAINER_SIZE, height: CONTAINER_SIZE }, style]}
+          >
+            <Svg width={CONTAINER_SIZE} height={CONTAINER_SIZE}>
+              <AnimatedPath animatedProps={animatedProps} />
+            </Svg>
+          </Animated.View>
+        </TapGestureHandler>
+      </Animated.View>
 
       <StatusBar style="auto" />
     </View>
@@ -87,7 +130,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingHorizontal: 15,
+    paddingHorizontal: PADDING_HORIZONTAL,
   },
   button_container: {
     width: '100%',
@@ -95,8 +138,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     borderWidth: StyleSheet.hairlineWidth,
-    // paddingHorizontal: 15,
-    // paddingVertical: 15,
     borderRadius: 12,
   },
 });
